e168b28e40a40853897610225dbf774f9c9c370e
Ochanda-Charles <ochanda.charles.16@gmail.com>
2023-07-31 19:16:03 +0300

    Carrying out basic arithmetics

---
+++ Basics.js
@@ -0,0 +1,34 @@
+//Output
+//Console is part of the web browser and allows you to log in messages, run js code and see errors and warnings
+console.log("Hello World!");
+
+//Operators and strings 
+//====Mathematical operators======//
+let x = 8;
+let y = 3;
+console.log(x+y);//addition
+console.log(x-y);//subtraction
+console.log(x*y);//multiplication
+console.log(x/y);//division
+console.log(x**y);//exponentation
+console.log(balls % boxes);//modulo operator//output the remainder after carrying out division
+
+
+//Increment and decrement operators
+let count = 0;
+count++;
+console.log(count);//returns 1
+//post-increment and post-increment
+//they return the value of the number before the actual operation has been carried out
+let p = 1; 
+console.log(p++);
+
+//post-decrement and pre-decrement
+//they return the value after the actual operation has taken place 
+
+let r  = 0;
+console.log(++r);//returns 1
+
+
+
+
