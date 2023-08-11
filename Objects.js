d52d78b29d93ba5faa951fd908e5e6af4fbd542a
Ochanda-Charles <ochanda.charles.16@gmail.com>
2023-08-11 18:00:35 +0300

    covered more on objects

--- Objects.js
+++ Objects.js
@@ -50,11 +50,14 @@ const myAnimal = new Animal("Lion", "Mammal" , "23");
 //Objects can be returned from functions.
 
 
+//When creating an object by using duplicate property names, the last property will overwrite the prior ones of the same name.
+var a = {x: 1, x: 2, x: 3, x: 4};
+
 //====Object Destructuring=====//
 //Object can be destructured so as some properties of the object can be assigned to variables
-//Think of it as unpacking variables  
+//Think of it as unpacking properties from an object and packing them into variables 
 const {animalName, animalSpecies} = myAnimal;
-console.log(snimalName);//Lion
+console.log(animalName);//Lion
 console.log(animalSpecies);//Mammal
 
 
@@ -68,3 +71,61 @@ myAnimal.species= "Mammal";
 delete myAnimal.age;
 
 
+//object assign() method 
+//allows us to combine multiple sources into one target to create a single new object. 
+//In the example below we created a new object (newStudent) that took some properties of both the person and student objects
+//There are no limitations on the number of source parameters. However, 
+//order is important because properties in the second parameter will be overridden by properties of the same name in third parameter,and so on.
+let person = {
+    name: 'Jack',
+    age: 18,
+    sex: 'male'
+};
+let student = {
+    name: 'Bob',
+    age: 20,
+    xp: '2'
+};
+//the first parameter is normally the target then every parameter after the first will be used as sources for the target
+let newStudent = Object.assign({}, person, student);
+
+console.log(newStudent.name); // Bob
+console.log(newStudent.age); // 20
+console.log(newStudent.sex); // male
+console.log(newStudent.xp); // 2
+
+
+//You can also use object.assign() to duplicate objects
+let person = {
+    name: 'Jack',
+    age: 18
+  };
+  
+  let newPerson = Object.assign({}, person); 
+  newPerson.name = 'Bob';
+  
+  console.log(person.name); // Jack
+  console.log(newPerson.name); // Bob
+
+
+//You can assign a value to an object property in the Object.assign() statement.
+  let person = {
+    name: 'Jack',
+    age: 18
+  };
+  
+  let newPerson = Object.assign({}, person, {name: 'Bob'});
+  //==Always remember that the more objects you add the more they overwrite the first objects
+
+
+
+  //==indexOf() function==//
+  //returns the index of the first substring
+  //if the substring is not found it returns -1 instead 
+  const str = "foobarbaz";
+  const substring = "bar";
+  const index = str.indexOf(substring);
+    console.log(index); // 3
+
+
+//
